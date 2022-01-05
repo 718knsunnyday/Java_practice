50e4d83d86cc5e854f4564124dc065f906f312b7
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-01-05 21:52:19 +0900

    [Add]Chapter5

--- wcp/Chapter4.java
+++ wcp/Chapter4.java
@@ -1,9 +1,16 @@
 public class Chapter4 {
   public static void main(String[] args){
-    int num;
-    num = 10;
+    String lang;
+    lang = "Korean";
+    System.out.println(lang);
+    final int NUM = 7;
+    System.out.println(NUM);
+    long num;
+    num = 56789L;
     System.out.println(num);
-    final String WORD = "LIKE";
-    System.out.println(WORD);
+    double double1 = 123.45d;
+    System.out.println(double1);
+    boolean boolean1 = true;
+    System.out.println(boolean1);
   }
 }
