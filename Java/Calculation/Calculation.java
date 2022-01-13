4b2ebbb6ac59fb4b9dcddad07575c92f176afe29
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-01-13 11:38:52 +0900

    [Add]Calculation

---
+++ Java/Calculation/Calculation.java
@@ -0,0 +1,20 @@
+public class Calculation {
+  public static void main(String[] args){
+    double a = 9.0/2;
+    System.out.println(a);
+    int b = 1000;
+    b++ ;
+    System.out.println(b);
+    int c = 1000;
+    c-- ;
+    System.out.println(c);
+    b += 10;
+    System.out.println(b);
+    double e = 8.5*2;
+    System.out.println(e);
+    double f = 8.5/2;
+    System.out.println(f);
+    double g = 7;
+    System.out.println(g);
+  }
+}
