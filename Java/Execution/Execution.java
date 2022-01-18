cc5c3337117062eca8da550f4b7860c3b6d11e70
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-01-18 22:41:27 +0900

    [Add]練習

--- Java/Execution/Execution.java
+++ Java/Execution/Execution.java
@@ -20,8 +20,23 @@ public class Execution{
     System.out.println(i);
     System.out.println("あなたの出身地はどこですか？");
     String prefecture = new java.util.Scanner(System.in).nextLine();
+    System.out.println(prefecture);
     System.out.println("何歳ですか？");
     int j = new java.util.Scanner(System.in).nextInt();
+    System.out.println(j);
     System.out.println(prefecture + "出身の" + j + "歳です。");
+    System.out.println("ようこそ");
+    System.out.println("あなたの名前を入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println(name);
+    System.out.println("あなたの年齢を入力してください");
+    String z = new java.util.Scanner(System.in).nextLine();
+    int x = Integer.parseInt(z);
+    int fortune = new java.util.Random().nextInt(3);
+    fortune++;
+    System.out.println("占いの結果が出ました！");
+    System.out.println(x + "歳の" + name + "さんの運気番号は" + fortune + "です");
+    System.out.println("1: 大吉 2:中吉 3:吉 4:凶" );
   } 
 }
+
