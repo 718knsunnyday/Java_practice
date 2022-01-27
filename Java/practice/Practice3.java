ecb1445a6252e39d05e3ddb975896817bdc990e2
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-01-27 12:36:28 +0900

    [Add]practice

--- Java/practice/Practice3.java
+++ Java/practice/Practice3.java
@@ -2,7 +2,7 @@ public class Practice3 {
   public static void main(String[] args){
     System.out.println("[数あてゲーム]");
     int ans = new java.util.Random().nextInt(9);
-    for (ans = 0; ans < 5 ; ans++){
+    for (int i = 0; i < 5 ; i++){
       System.out.println("0-9の数字を入れて下さい");
       int num = new java.util.Scanner(System.in).nextInt();
       if (num == ans){
---
+++ Java/practice/Practice4.java
@@ -0,0 +1,11 @@
+public class Practice4{
+  public static void main(String[] args){
+    int[] moneyList = {121902, 8302, 55100};
+    for ( int i = 0; i < moneyList.length; i++){
+      System.out.println(moneyList[i]);
+    }
+    for ( int num : moneyList){
+      System.out.println(num);
+    }
+  }
+}
