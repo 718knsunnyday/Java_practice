1affe1258c4d14b63bb01b777c3601c26a6ec3fc
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-01-27 12:09:55 +0900

    [Add]配列練習

---
+++ Java/Array/Array4.java
@@ -0,0 +1,8 @@
+public class Array4{
+  public static void main(String[] args){
+    int[][] scores = new int [2][3];
+    scores [0][0] = 20;
+    scores [0][1] = 30;
+    System.out.println(scores[0][0]);
+  }
+}
