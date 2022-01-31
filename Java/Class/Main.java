21496bf74571d807310566293cecabd4d4e169ca
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-01-31 22:58:17 +0900

    [Add]Class

--- Java/Class/Main.java
+++ Java/Class/Main.java
@@ -5,16 +5,3 @@ public class Main{
     h.hp = 100;
   }
 }
-
-
-
-public class Hero{
-  String name;
-  int hp;
-  int level = 10;
-
-  public void sleep() {
-    this.hp = 100;
-    System.out.println(this.name + "は、眠って回復した！")；
-  }
-}
