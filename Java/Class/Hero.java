7485767631c0ab2e47742d218373ebee2b023e33
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-02-03 10:50:15 +0900

    [Update]Mainファイル

--- Java/Class/Hero.java
+++ Java/Class/Hero.java
@@ -5,6 +5,6 @@ public class Hero{
 
   public void sleep() {
     this.hp = 100;
-    System.out.println(this.name + "は、眠って回復した！")；
+    System.out.println(this.name + "は、眠って回復した");
   }
 }
--- Java/Class/Main.java
+++ Java/Class/Main.java
@@ -3,5 +3,6 @@ public class Main{
     Hero h = new Hero();
     h.name = "中野";
     h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出した");
   }
 }
