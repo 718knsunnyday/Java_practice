0b4f2c9f1b5d5bb398accdf8c29adf5611179a1b
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-02-04 12:50:42 +0900

    [Add]Class

---
+++ Java/Class3/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+  public static void main(String[] orgs){
+    Sword s = new Sword();
+    s.name = "炎の剣";
+    s.damage = 10;
+    Hero h = new Hero();
+    h.name = "湊";
+    h.hp = 100;
+    h.sword = s;
+    System.out.println("現在の武器は" + h.sword.name);
+    Wizard w = new Wizard();
+    w.name = "魔女";
+    w.hp = 50;
+    w.heal(h);
+  }
+}
